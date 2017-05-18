71dd120ca4559cc8a9b8b5ea10a261d92fe0559b
Luthfi Kurnia Putra <luthfiswees@gmail.com>
2017-05-19 01:49:40 +0700

    Add ready set go

--- public/javascripts/time.js
+++ public/javascripts/time.js
@@ -17,20 +17,49 @@ window.onbeforeunload = function (e) {
 // Countdown the timer
 $(document).ready(function(){
   setInitialTime(10);
-
+  var boolean_go     = false;
+  var boolean_ready  = false;
+  var boolean_timer  = false;
+  var start          = false;
+  var prepare = setInterval(function(){
+    if (!boolean_ready){
+      $("#ilm-start").html("Ready");
+      $("#ilm-start").show();
+      boolean_ready = true;
+    } else if (!boolean_timer) {
+      $("#ilm-start").hide();
+      $("#ilm-start").html("Timer Set");
+      $("#ilm-start").show();
+      boolean_timer = true;
+    } else if (!boolean_go) {
+      $("#ilm-start").hide();
+      $("#ilm-start").html("Go!");
+      $("#ilm-start").show();
+      boolean_go = true;
+    } else {
+      $("#ilm-start").hide();
+      if (!start){
+        setInitialTime(10);
+      }
+      start = true;
+    }
+  },1500);
   var timer = setInterval(function() {
 
     var count = parseInt($('#ilm-time-box-value').html());
-    if (count !== 0 && !win) {
+    if (count !== 0 && !win && start) {
       $('#ilm-time-box-value').html(count - 1);
     } else {
-      clearInterval(timer);
-      out = true;
+      if (start && boolean_ready && boolean_timer && boolean_go) {
+        clearInterval(timer);
+        out = true;
 
-      // Show up the popup
-      if (!win){
-          $('#ilm-modal-box-lose').show();
+        // Show up the popup
+        if (!win){
+            $('#ilm-modal-box-lose').show();
+        }
       }
     }
+
   }, 1000);
 });
--- public/javascripts/tutorial-time.js
+++ public/javascripts/tutorial-time.js
@@ -18,19 +18,48 @@ window.onbeforeunload = function (e) {
 $(document).ready(function(){
   $('#close-modal-box-introduction-3').click(function(event){
     setInitialTime(10);
+    var boolean_go     = false;
+    var boolean_ready  = false;
+    var boolean_timer  = false;
+    var start          = false;
+    var prepare = setInterval(function(){
+      if (!boolean_ready){
+        $("#ilm-start").html("Ready");
+        $("#ilm-start").show();
+        boolean_ready = true;
+      } else if (!boolean_timer) {
+        $("#ilm-start").hide();
+        $("#ilm-start").html("Timer Set");
+        $("#ilm-start").show();
+        boolean_timer = true;
+      } else if (!boolean_go) {
+        $("#ilm-start").hide();
+        $("#ilm-start").html("Go!");
+        $("#ilm-start").show();
+        boolean_go = true;
+      } else {
+        $("#ilm-start").hide();
+        if (!start){
+          setInitialTime(10);
+        }
+        start = true;
+      }
+    },1500);
 
     var timer = setInterval(function() {
 
       var count = parseInt($('#ilm-time-box-value').html());
-      if (count !== 0 && !win) {
+      if (count !== 0 && !win && start) {
         $('#ilm-time-box-value').html(count - 1);
       } else {
-        clearInterval(timer);
-        out = true;
+        if (start && boolean_ready && boolean_timer && boolean_go) {
+          clearInterval(timer);
+          out = true;
 
-        // Show up the popup
-        if (!win){
-            $('#ilm-modal-box-lose').show();
+          // Show up the popup
+          if (!win){
+              $('#ilm-modal-box-lose').show();
+          }
         }
       }
     }, 1000);
